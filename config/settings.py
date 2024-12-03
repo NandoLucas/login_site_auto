0dfcc69f86194ff794cab2c2de235077fef101bf
NandoLucas <nandolukss@gmail.com>
2024-12-03 12:11:53 -0300

    feat: Criação da Estrutura de pastas do projeto
    Criação da estrutura de pastas visando a utilização de containers docker para desenvolvimento.

---
+++ config/settings.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+# Carregar variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Configurações do site e elementos
+BASE_URL = os.getenv("BASE_URL", "https://example.com/login")
+USERNAME_FIELD_ID = os.getenv("USERNAME_FIELD_ID", "username_input_id")
+PASSWORD_FIELD_ID = os.getenv("PASSWORD_FIELD_ID", "password_input_id")
+LOGIN_BUTTON_ID = os.getenv("LOGIN_BUTTON_ID", "login_button_id")
+
+# Credenciais
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
